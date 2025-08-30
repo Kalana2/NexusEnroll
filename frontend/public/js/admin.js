7100de9335a112db712d70b93e7be44d60ab28c5
VNavodi <videeshanavodi1234@gmail.com>
2025-08-30 19:56:35 +0530

    Added admin

---
+++ frontend/public/js/admin.js
@@ -0,0 +1,23 @@
+const overlay = document.getElementById('overlay');
+    const openBtn = document.getElementById('openOverlay');
+    const closeBtn = document.getElementById('closeOverlay');
+
+    openBtn.onclick = () => overlay.style.display = 'flex';
+    closeBtn.onclick = () => overlay.style.display = 'none';
+
+
+
+    function confirmDelete() { 
+            document.getElementById('deleteModal').style.display = 'block';
+        }
+function cancelDelete() {
+            currentItemToDelete = null;
+            document.getElementById('deleteModal').style.display = 'none';
+        }
+           window.onclick = function(event) {
+            const modal = document.getElementById('deleteModal');
+            if (event.target === modal) {
+                cancelDelete();
+            }
+        }
+        
