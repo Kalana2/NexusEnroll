61d148cb8ed53a860f927de0b21209947b2717f0
Kalana <kalanajinendra@gmail.com>
2025-09-05 16:48:23 +0530

    add course-service class model

--- backend/course-service/src/main.py
+++ backend/course-service/src/main.py
@@ -6,6 +6,11 @@ from models.course import Course, CourseUpdate
 from models.courseTrend import CourseTrend
 from services.courseService import CourseService
 from services.prerequisiteService import PrerequisiteService
+from supabase import create_client, Client
+
+SUPABASE_URL = "https://gcepytafvxmgddfrhpah.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjZXB5dGFmdnhtZ2RkZnJocGFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwNjA2NTAsImV4cCI6MjA3MjYzNjY1MH0.vE3i9vOh2ZItBE4zp7FcCvoEOmtCdU4_MkUZSB4MhTo"
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI(title="Course Service", version="1.0.0")
 
